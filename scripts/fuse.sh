b7b752beea783eeb07229a9a5faf7eb332decc38
Yohanes Widi Sono <yohanesws@Yohaness-MacBook-Pro.local>
2017-02-10 18:35:41 +0700

    maven url config

--- scripts/fuse.sh
+++ scripts/fuse.sh
@@ -1,5 +1,6 @@
 echo "============ Unzipping fuse (2/2)============ "
 unzip /vagrant/binary/jboss-fuse-karaf-6.3.0.redhat-187.zip -d /home/vagrant/
 sudo chmod -R 777 /home/vagrant/jboss-fuse-6.3.0.redhat-187
+sudo cp -a /vagrant/config/all/. /home/vagrant/jboss-fuse-6.3.0.redhat-187/etc/
 #echo "============ configuring fuse environment (5/5)============"
 #echo "source /vagrant/binary/.bashrc" >> /home/vagrant/.bashrc
